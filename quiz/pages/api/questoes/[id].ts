001a294398d336e7c7f003918c1af52c021b2d61
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-01-31 00:52:29 -0300

    criação de componentes, css e do temporizador

--- quiz/pages/api/questoes/[id].ts
+++ quiz/pages/api/questoes/[id].ts
@@ -2,20 +2,13 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import QuestaoModel from '../../../model/questao'
 import questoes from '../bancoDeQuestoes'
 
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<QuestaoModel>
-) {
+export default function handler(req: any, res: any) {
   const idSelecionado = +req.query.id
 
   const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado)
 
   if(unicaQuestaoOuNada.length === 1){
-    const questaoSelecionada = unicaQuestaoOuNada[0]
+    const questaoSelecionada = unicaQuestaoOuNada[0].embaralharRespostas()
 
     res.status(200).json(questaoSelecionada.paraObjeto())
   } else{
