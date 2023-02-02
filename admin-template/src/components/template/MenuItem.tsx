f796bc84983362f767f19e1d731847f52de4186e
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-02-02 17:49:59 -0300

    configuração do modo dark

--- admin-template/src/components/template/MenuItem.tsx
+++ admin-template/src/components/template/MenuItem.tsx
@@ -1,27 +1,43 @@
 import Link from "next/link"
 
 interface MenuItemProps{
-    url: string
     texto: string
     icone: any
+    url?: string
+    className?: string
+    onClick?: (evento: any) => void
 }
 
 export default function MenuItem(props:MenuItemProps){
+    function renderizarLink(){
+        return (
+            <span className={`
+                flex flex-col justify-center items-center
+                h-20 w-20 text-gray-600 
+                dark:text-gray-200
+                ${props.className}
+            `}>
+                {props.icone}
+                <span className={`
+                    text-xs font-light 
+                `}>
+                    {props.texto}
+                </span>
+            </span>
+        )
+    }
+
     return(
-        <li className={`hover:bg-gray-100`}>
-            <Link href={props.url}>
-                    <span className={`
-                        flex flex-col justify-center items-center
-                        h-20 w-20
-                    `}>
-                        {props.icone}
-                        <span className={`
-                            text-xs font-light text-gray-600
-                        `}>
-                            {props.texto}
-                        </span>
-                    </span>
-            </Link>
+        <li onClick={props.onClick} className={`
+            hover:bg-gray-100 dark:hover:bg-gray-800
+            cursor-pointer
+            `}>
+            {props.url ? (
+                <Link href={props.url}>
+                    {renderizarLink()}
+                </Link>
+            ): (renderizarLink())}
+            
         </li>
     )
 }
--- admin-template/src/components/template/MenuLateral.tsx
+++ admin-template/src/components/template/MenuLateral.tsx
@@ -1,21 +1,32 @@
 import MenuItem from "./MenuItem";
-import { IconeAjustes, IconeCasa, IconeSino } from "../icons";
+import { IconeAjustes, IconeCasa, IconeSair, IconeSino } from "../icons";
 import Logo from "./Logo";
 
 export default function MenuLateral(){
     return(
-        <aside>
+        <aside className={`
+            flex flex-col
+            bg-gray-200 text-gray-700
+            dark:bg-gray-900 
+        `}>
             <div className={`
-            flex flex-col items-center justify-center
+                flex flex-col items-center justify-center
                 bg-gradient-to-r from-indigo-500 0 to-purple-800
                 h-20 w-20
             `}>
                 <Logo />
             </div>
-            <ul>
+            <ul className="flex-grow">
                 <MenuItem url="/" texto="Início" icone={IconeCasa} />
                 <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
                 <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
+            </ul>
+            <ul>
+                <MenuItem onClick={() => console.log('logout')} texto="Sair" icone={IconeSair} className={`
+                    text-red-600 dark:text-red-400
+                    hover:bg-red-400 hover:text-white 
+                    dark:hover:text-white
+                `} />
             </ul>
         </aside>
     )
