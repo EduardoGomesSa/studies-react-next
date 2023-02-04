62fe1127f312cad36129696cc3c99ea34fdeede0
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-02-03 23:05:12 -0300

    criação do login com a conta google

---
+++ admin-template/src/pages/perfil.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/template/Layout";
+
+export default function Perfil() {
+  return (
+    <Layout titulo="Perfil do Usuário" subtitulo="Aqui ficará as informações do usuário">
+      <h1>Perfil</h1>
+    </Layout>
+  )
+}
